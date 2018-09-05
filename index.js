d771b66bae0ef68d27cea1e12694a9a07ad00553
Larissa-Fabiana <ferreira-larissa@outlook.com>
2018-09-05 18:33:34 -0300

    função principal refatorada

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 module.exports.getLinksFromMd = getLinksFromMd;
 
-function getLinksFromMd(str){
-  let correctString = validateString(str);
-  let markdowns = markdownsSelection(correctString);
-  let finalArray = transformMarkdowInArrayOfObjects(markdowns);
-  return finalArray;
-}
-function validateString(mensage){
+function getLinksFromMd(mensage){
   if(typeof mensage === 'number'){
     throw new Error('Insira apenas strings');
   }else if( mensage === ''){
     throw new Error('Insira pelo menos um markdown');
   }else{
-    return mensage;
+    return markdownsSelection(mensage);
   }
 }
+
 function markdownsSelection(str){
-  let regex = /(\[\w+\W*\s*\w*\])\((http(s)*:\/\/)*\w+(\.*\w\/*){1,}\)/g;
-  let array = str.match(regex);
-  return array;
+  const regex = /(\[\w+\W*\s*\w*\])\((http(s)*:\/\/)*\w+(\.*\w\/*){1,}\)/g;
+  const array = str.match(regex);
+  return transformMarkdowInArrayOfObjects(array);
 }
+
 function transformMarkdowInArrayOfObjects(array){
   let arrayOfObjects = [];
   for(let a in array){
     let obj = {};
-    let regexText = (array[a]).match(/(\[.*\])*/g);
-    let text = (regexText[0]).replace(/([\[\]])*/g, '');
-    let regexHref = (array[a]).match(/(\(.*\))/g);
-    let href = (regexHref[0]).replace(/([\(\)])*/g, '');
-    obj['href'] = href;
-    obj['text'] = text;	
+    const regex = /\[(.*)\]\((.*)\)/;
+    let arrayTextAndHref = (array[a]).match(regex);
+    obj['href'] = arrayTextAndHref[2];
+    obj['text'] = arrayTextAndHref[1];
     arrayOfObjects.push(obj);
   }
   return arrayOfObjects;
